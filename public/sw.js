63ed564bc8cb89f6d478b6887eae458a3dd919ef
Lysenko <lysenkoden@gmail.com>
2024-06-14 22:55:38 +0200

    fixed sw.js-04

--- public/sw.js
+++ public/sw.js
@@ -1,65 +1,44 @@
 /* eslint-disable no-restricted-globals */
 
-const CACHE_NAME = 'my-cache-v1';
-const urlsToCache = ['/', 'index.html', 'static/js/bundle.js'];
+const CACHE_NAME = 'my-cache';
+const urlsToCache = [
+  '/',
+  'index.html',
+  'static/css/main.ddf4afa8.css',
+  'static/js/main.54cd1718.js',
+  'static/media/2021-11-15-2.31a24c4f285e134d9853.jpg',
+  'static/media/bg-01.d909c4d18bde432222e3.png',
+  'static/media/h-01.b55750694ac8e72ab2c8.png',
+  'h-02.d841a972af327501b510.png',
+];
 
 self.addEventListener('install', (event) => {
-  console.log('Service Worker: Installing');
-
+  console.log('installing SW');
   event.waitUntil(
     caches
       .open(CACHE_NAME)
       .then((cache) => {
-        console.log('Service Worker: Caching Files');
         return cache.addAll(urlsToCache);
       })
-      .then(() => {
-        console.log('Service Worker: Installation completed');
-        return self.skipWaiting();
-      })
-      .catch((error) => {
-        console.error('Service Worker: Installation failed', error);
-      })
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener('activate', (event) => {
-  console.log('Service Worker: Activating');
-
-  event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) => {
-        return Promise.all(
-          cacheNames.map((cache) => {
-            if (cache !== CACHE_NAME) {
-              console.log('Service Worker: Clearing Old Cache', cache);
-              return caches.delete(cache);
-            }
-          })
-        );
-      })
-      .then(() => {
-        console.log('Service Worker: Activation completed');
-        return self.clients.claim();
-      })
-  );
+  console.log('activating SW');
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
-  console.log('Service Worker: Fetching', event.request.url);
-
+  console.log(`fetching ${event.request.url}`);
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        console.log('Service Worker: Cache hit for', event.request.url);
-        return response; // return cached response if found
-      }
-
-      console.log('Service Worker: Cache miss for', event.request.url);
-
-      return fetch(event.request)
-        .then((response) => {
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          return response; // return cached response if found
+        }
+        return fetch(event.request).then((response) => {
           if (
             !response ||
             response.status !== 200 ||
@@ -75,11 +54,11 @@ self.addEventListener('fetch', (event) => {
           });
 
           return response;
-        })
-        .catch(() => {
-          // Fallback for failed network requests
-          return caches.match('index.html');
         });
-    })
+      })
+      .catch(() => {
+        // Fallback in case of a network error and resource is not cached
+        return caches.match('index.html');
+      })
   );
 });
