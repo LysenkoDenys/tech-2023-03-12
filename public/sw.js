81ba231f117f27609054989166204970062155f6
Lysenko <lysenkoden@gmail.com>
2024-06-14 22:12:21 +0200

    fix sw

--- public/sw.js
+++ public/sw.js
@@ -1,49 +1,13 @@
-// const CACHE_NAME = 'my-cache';
-// self.addEventListener('install', (e) => {
-//   console.log('installing SW'); //
-//   e.waitUntil(
-//     caches.open(CACHE_NAME).then((cache) => {
-//       return cache
-//         .addAll(['/', 'index.html', 'static/js/bundle.js'])
-//         .then(() => self.skipWaiting());
-//     })
-//   );
-// });
-
-// self.addEventListener('activate', (event) => {
-//   console.log('activating SW'); //
-//   event.waitUntil(self.clients.claim());
-// });
-
-// self.addEventListener('fetch', function (event) {
-//   console.log(`fetching ${event.request.url}`); //
-//   if (navigator.online) {
-//     var fetchRequest = event.request.clone();
-//     return fetch(fetchRequest).then(function (response) {
-//       if (!response || response.status !== 200 || response.type !== 'basic') {
-//         return response;
-//       }
-
-//       var responseToCache = response.clone();
-
-//       caches.open(CACHE_NAME).then(function (cache) {
-//         cache.put(event.request, responseToCache);
-//       });
-//       return response;
-//     });
-//   } else {
-//     event.respondWith(
-//       caches.match(event.request).then(function (response) {
-//         if (response) {
-//           return response;
-//         }
-//       })
-//     );
-//   }
-// });
+/* eslint-disable no-restricted-globals */
 
 const CACHE_NAME = 'my-cache';
-const urlsToCache = ['/', 'index.html', 'static/js/bundle.js'];
+const urlsToCache = [
+  '/',
+  'index.html',
+  'static/js/bundle.js',
+  '/static/js/main.chunk.js',
+  '/static/js/0.chunk.js',
+];
 
 self.addEventListener('install', (event) => {
   console.log('installing SW');
